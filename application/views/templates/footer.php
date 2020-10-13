09c3292557755f5cd043e79140e71e366ccb9096
mohrahmatullah <mohrahmatullah17051992@gmail.com>
2020-10-13 16:29:18 +0700

    first

---
+++ application/views/templates/footer.php
@@ -0,0 +1,11 @@
+	<script src="<?php echo base_url('assets/jquery/jquery-1.12.0.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#book-table').DataTable();
+		});
+	</script>
+</body>
+</html>
